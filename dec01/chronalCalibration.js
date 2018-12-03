1f632b4e810d2ff48bad5e7fbe46aa6e970541cf
Attila Gal <attila.gal@gmail.com>
2018-12-03 14:27:45 +0100

    dec01 - part 2

--- dec01/chronalCalibration.js
+++ dec01/chronalCalibration.js
@@ -3,4 +3,22 @@ const { readInputFile } = require('../utils/fileReader');
 const input = readInputFile('./chronalCalibration.input.txt');
 
 const result = input.reduce((acc, change) => acc + Number(change), 0);
-console.log(`solution = ${result}`);
+console.log(`PART 1: solution = ${result}`);
+
+function partTwo() {
+  const foundFrequencies = new Set([0]);
+  let currentFrequency = 0
+  while(true) {
+    for (let i = 0; i < input.length; i++) {
+      const modifier = input[i];
+      currentFrequency += Number(modifier);
+      if(foundFrequencies.has(currentFrequency)) {
+        return currentFrequency;
+      }
+      foundFrequencies.add(currentFrequency);
+    }
+  }
+}
+
+const resultPartTwo = partTwo();
+console.log(`PART 2: solution = ${resultPartTwo}`);
