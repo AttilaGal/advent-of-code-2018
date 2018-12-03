bd1cf8aceede83c803e4829fad386354ffe26e74
Attila Gal <attila.gal@gmail.com>
2018-12-03 21:08:48 +0100

    dec02-02

--- dec02/inventoryManagementSystem.js
+++ dec02/inventoryManagementSystem.js
@@ -26,4 +26,45 @@ function partOne() {
   return checkSumvariables.twice * checkSumvariables.thrice;
 }
 
+function findMatchingId(currentId, otherIds) {
+  return otherIds.find(other => {
+    let misMatchCount = 0;
+    other.split('').forEach((letter, index) => {
+      if(currentId[index] !== letter) {
+        ++misMatchCount;
+      }
+    });
+    if (misMatchCount > 1) {
+      return false;
+    }
+    return true;
+  });
+}
+
+function filterOutMismatch(currentId, matchingId) {
+  const current = currentId.split('');
+  const matching = matchingId.split('');
+  let misMatchIndex = null;
+  let i = 0;
+  while(misMatchIndex === null) {
+    if(current[i] !== matching[i]) {
+      misMatchIndex = i;
+    }
+    ++i;
+  }
+  current[misMatchIndex] = null;
+  return current.filter(n => n).join('');
+}
+
+function partTwo() {
+  for (let i = 0; i < input.length; i++) {
+    const currentId = input[i];
+    const matchingId = findMatchingId(currentId, input.slice(i + 1));
+    if(matchingId) {
+      return filterOutMismatch(currentId, matchingId);
+    }
+  }
+}
+
 console.log(`PART 1: solution = ${partOne()}`);
+console.log(`PART 2: solution = ${partTwo()}`);
