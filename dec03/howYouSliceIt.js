e73a74dbe8433405301b2f2330a52f32df260473
Attila Gal <attila.gal@gmail.com>
2018-12-04 19:17:54 +0100

    added jest + setup dec03

---
+++ dec03/howYouSliceIt.js
@@ -0,0 +1,10 @@
+const { readInputFile } = require('../utils/fileReader');
+const input = readInputFile('./dec03/input.txt');
+
+
+
+function partOne() {
+
+}
+
+console.log(`PART 1: solution = ${partOne()}`);
